4106bf4d5465bc70f2d0eaf8adc544a44d6b148b
jakebugeja <jake.bugeja.b42775@mcast.edu.mt>
2021-10-19 13:55:17 +0200

    Added a connection string inside the appsettings.son - to connect with database Installed EntityFrameworkCore.Sql server version 3.1.20 Installed EntityFrameworkCore.relational version 3.1.20 Converted the libriries to version 3.1.20
    Added a connection string inside the appsettings.son - to connect with database
    Installed EntityFrameworkCore.Sql server version 3.1.20
    Installed EntityFrameworkCore.relational version 3.1.20
    Converted the libriries to version 3.1.20

--- EX/ex1_part2/Solution1/DataAcess/Context/BloggingContext.cs
+++ EX/ex1_part2/Solution1/DataAcess/Context/BloggingContext.cs
@@ -4,9 +4,19 @@ using System;
 
 namespace DataAcess
 {
-    
+    //this is the class which is the gateway to our databse
+    //this inherits from a built in class called xxxDbContext
+    //the dbcontext class contains methods which will allowus to
+    //amongst other things to connect with the database; add an item
+    //to the database, delete, querying using LINQ...
     public class BloggingContext:DbContext
     {
+        public BloggingContext(DbContextOptions<BloggingContext>options)://this is a contructor
+            base(options)                                //options ais passed to the base class
+        {
+
+        }
+
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
 
