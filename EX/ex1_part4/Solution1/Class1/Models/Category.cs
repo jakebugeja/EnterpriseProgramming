0f2b637a84585255a7a4ee532423a4a4cc3b90d3
jakebugeja <jake.bugeja.b42775@mcast.edu.mt>
2021-10-25 10:02:23 +0200

    Completed lesson work

---
+++ EX/ex1_part4/Solution1/Class1/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Domain.Models
+{
+    //fields are normally private
+    //properties are often public
+    public class Category
+    {
+        //[Key ctrl+.] --> this will generate:using System.ComponentModel.DataAnnotations;
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
